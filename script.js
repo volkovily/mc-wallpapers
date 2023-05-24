a295e0e2cc5d11a0e608b454718b50ff090bb49d
Ilya <89909053+volkovily@users.noreply.github.com>
2023-05-24 12:42:51 +0200

    added canvas to display images

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const fileInput = document.getElementById("fileInput");
-const imageContainer = document.getElementById("imageContainer");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 fileInput.addEventListener("change", () => {
   const file = fileInput.files[0];
   const reader = new FileReader();
 
   reader.onload = () => {
-    imageContainer.innerHTML = `<img src="${reader.result}">`;
+    const img = new Image();
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = reader.result;
   };
-
   reader.readAsDataURL(file);
 });
