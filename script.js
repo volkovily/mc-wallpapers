367c6edfa092ccbb1b7a8fce81fdcca835ccf623
Ilya <89909053+volkovily@users.noreply.github.com>
2023-05-27 10:41:18 +0200

    added object for loaded images

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const fileInput = document.getElementById("fileInput");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let images = {};
+
 fileInput.addEventListener("change", () => {
   const files = fileInput.files;
+  console.log(images)
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
     const reader = new FileReader();
 
   reader.onload = () => {
-    const img = new Image();
+    let img = new Image();
+    let name = files[i].name;
+    images[name] = img;
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
