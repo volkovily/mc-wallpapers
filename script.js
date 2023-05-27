6fd11fa48a088d02a0f38864f7e81b92394a2db8
Ilya <89909053+volkovily@users.noreply.github.com>
2023-05-27 18:05:42 +0200

    fixed skin not being scaled properly

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ function merge(uvmap, skin, background, onRender) {
 }
 
 Caman.Filter.register("remap", function (image, uChannel, vChannel) {
+  let canvas = resizeSkin(image, 256 / image.width);
   let pixelData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height).data;
   this.process("remap", function (rgba) {
     let location = (rgba[vChannel] * canvas.width + rgba[uChannel]) * 4;
@@ -59,3 +60,12 @@ Caman.Filter.register("remap", function (image, uChannel, vChannel) {
     rgba.a = (rgba.a * pixelData[location + 3]) / 255;
   });
 });
+
+function resizeSkin(image, factor) {
+  canvas.width = image.width * factor;
+  canvas.height = canvas.width;
+
+  ctx.scale(factor, factor);
+  ctx.drawImage(image, 0, 0);
+  return canvas;
+}
