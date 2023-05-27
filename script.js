f0fd76182bc9e00421cbe337c64a8d3e0181c8a8
Ilya <89909053+volkovily@users.noreply.github.com>
2023-05-27 16:44:58 +0200

    added remap filter
    This remap filter using Caman.js remaps the colors of an image based on the UV channels of each pixel.

--- script.js
+++ script.js
@@ -6,22 +6,32 @@ let images = {};
 
 fileInput.addEventListener("change", () => {
   const files = fileInput.files;
-  console.log(images)
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
     const reader = new FileReader();
 
   reader.onload = () => {
     let img = new Image();
-    let name = files[i].name;
+    let name = file.name.replace(".png", "");
     images[name] = img;
+    img.src = URL.createObjectURL(file);
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
     };
-    img.src = reader.result;
   };
   reader.readAsDataURL(file);
   }
+});
+
+Caman.Filter.register("remap", function (image, uChannel, vChannel) {
+  let pixelData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height).data;
+  this.process("remap", function (rgba) {
+    let location = (rgba[vChannel] * canvas.width + rgba[uChannel]) * 4;
+    rgba.r = (rgba.a * pixelData[location + 0]) / 255;
+    rgba.g = (rgba.a * pixelData[location + 1]) / 255;
+    rgba.b = (rgba.a * pixelData[location + 2]) / 255;
+    rgba.a = (rgba.a * pixelData[location + 3]) / 255;
+  });
 });
