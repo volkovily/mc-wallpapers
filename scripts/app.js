6f3f70156946bd73da5e0a59a0e6a02880203136
Ilya <89909053+volkovily@users.noreply.github.com>
2023-06-03 11:08:54 +0200

    added support for TL skins

--- scripts/app.js
+++ scripts/app.js
@@ -16,22 +16,30 @@ skin.src = "src/img/steve_new.png";
 
 const OLD_FORMAT_HEIGHT = 32;
 
-const playerNameButton = document.getElementById("getSkinBtn");
+const mojangSkinButton = document.getElementById("getMojangSkinBtn");
+const tlSkinButton = document.getElementById("getTLSkinBtn");
 const playerNameInput = document.getElementById("playerNameInput");
 const skinHistoryContainer = document.getElementById("skinHistoryContainer");
 const skinHistoryMap = new Map();
 
-playerNameButton.addEventListener("click", () => {
+mojangSkinButton.addEventListener("click", () => {
   let playerName = playerNameInput.value;
   if (playerName.trim() !== "") {
     getUUID(playerName).then((uuid) => {
-      loadSkin(uuid);
+      loadMojangSkin(uuid);
       addSkinToHistory(uuid);
     });
   }
 });
 
-function loadSkin(uuid) {
+tlSkinButton.addEventListener("click", () => {
+  let playerName = playerNameInput.value;
+  if (playerName.trim() !== "") {
+    loadTLSkin(playerName);
+  }
+});
+
+function loadMojangSkin(uuid) {
   skin.src = `https://visage.surgeplay.com/skin/${uuid}`;
   skin.crossOrigin = "anonymous";
   skin.onload = () => {
@@ -46,25 +54,33 @@ function loadSkin(uuid) {
   };
 }
 
+function loadTLSkin(playerName) {
+  skin.src = `https://auth.tlauncher.org/skin/fileservice/skins/skin_${playerName}.png`;
+  skin.crossOrigin = "anonymous";
+  skin.onload = () => {
+    compose();
+  };
+}
+
 function addSkinToHistory(uuid) {
-    if (!uuid) {
+  if (!uuid) {
     return;
   }
   if (skinHistoryMap.has(uuid)) {
     const existingItem = skinHistoryMap.get(uuid);
     skinHistoryContainer.insertBefore(existingItem, skinHistoryContainer.firstChild);
   } else {
-  const historyItem = document.createElement("div");
-  historyItem.classList.add("history-item");
-  const skinPreviewBtn = document.createElement("button");
-  skinPreviewBtn.classList.add("skin-preview-button");
-  skinPreviewBtn.style.backgroundImage = `url(https://visage.surgeplay.com/front/128/${uuid})`;
-  skinPreviewBtn.addEventListener("click", () => {
-    loadSkin(uuid);
-  });
-  historyItem.appendChild(skinPreviewBtn);
-  skinHistoryContainer.insertBefore(historyItem, skinHistoryContainer.firstChild);
-    
+    const historyItem = document.createElement("div");
+    historyItem.classList.add("history-item");
+    const skinPreviewBtn = document.createElement("button");
+    skinPreviewBtn.classList.add("skin-preview-button");
+    skinPreviewBtn.style.backgroundImage = `url(https://visage.surgeplay.com/front/128/${uuid})`;
+    skinPreviewBtn.addEventListener("click", () => {
+      loadMojangSkin(uuid);
+    });
+    historyItem.appendChild(skinPreviewBtn);
+    skinHistoryContainer.insertBefore(historyItem, skinHistoryContainer.firstChild);
+
     skinHistoryMap.set(uuid, historyItem);
   }
 }
