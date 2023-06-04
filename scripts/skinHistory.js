0ec60b5bb5aee8ad1645bd70597c4b84f96da2e2
Ilya <89909053+volkovily@users.noreply.github.com>
2023-06-04 14:55:27 +0200

    split `addSkinToHistory` into smaller functions

--- scripts/skinHistory.js
+++ scripts/skinHistory.js
@@ -2,24 +2,40 @@ const skinHistoryContainer = document.getElementById("skinHistoryContainer");
 const skinHistoryMap = new Map();
 
 export function addSkinToHistory(uuid) {
-    if (!uuid) {
-      return;
-    }
-    if (skinHistoryMap.has(uuid)) {
-      const existingItem = skinHistoryMap.get(uuid);
-      skinHistoryContainer.insertBefore(existingItem, skinHistoryContainer.firstChild);
-    } else {
-      const historyItem = document.createElement("div");
-      historyItem.classList.add("history-item");
-      const skinPreviewBtn = document.createElement("button");
-      skinPreviewBtn.classList.add("skin-preview-button");
-      skinPreviewBtn.style.backgroundImage = `url(https://visage.surgeplay.com/front/128/${uuid})`;
-      skinPreviewBtn.addEventListener("click", () => {
-        loadMojangSkin(uuid);
-      });
-      historyItem.appendChild(skinPreviewBtn);
-      skinHistoryContainer.insertBefore(historyItem, skinHistoryContainer.firstChild);
-  
-      skinHistoryMap.set(uuid, historyItem);
-    }
+  if (!uuid) return
+
+  if (skinHistoryMap.has(uuid)) {
+    moveExistingItemToTop(uuid);
+  } else {
+    createAndInsertNewItem(uuid);
   }
+}
+
+function moveExistingItemToTop(uuid) {
+  const existingItem = skinHistoryMap.get(uuid);
+  skinHistoryContainer.insertBefore(existingItem, skinHistoryContainer.firstChild);
+}
+
+function createAndInsertNewItem(uuid) {
+  const historyItem = createHistoryItem(uuid);
+  skinHistoryContainer.insertBefore(historyItem, skinHistoryContainer.firstChild);
+  skinHistoryMap.set(uuid, historyItem);
+}
+
+function createHistoryItem(uuid) {
+  const historyItem = document.createElement("div");
+  historyItem.classList.add("history-item");
+  const skinPreviewBtn = createSkinPreviewButton(uuid);
+  historyItem.appendChild(skinPreviewBtn);
+  return historyItem;
+}
+
+function createSkinPreviewButton(uuid) {
+  const skinPreviewBtn = document.createElement("button");
+  skinPreviewBtn.classList.add("skin-preview-button");
+  skinPreviewBtn.style.backgroundImage = `url(https://visage.surgeplay.com/front/128/${uuid})`;
+  skinPreviewBtn.addEventListener("click", () => {
+    loadMojangSkin(uuid);
+  });
+  return skinPreviewBtn;
+}
